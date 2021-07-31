ae4301a98be12ba63058cbaf3609c8e477dd223d
阳之幸福 <1689484241@qq.com>
2021-07-31 21:59:46 +0800

    Sat Jul 31 21:59:25 CST 2021 update go2021 day03 update strs file

--- src/go2021/devops/day03/strs.go
+++ src/go2021/devops/day03/strs.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"unsafe"
 )
 
 func main1() {
@@ -32,9 +33,54 @@ func main4() {
 	exec.Command(cmd).Run()
 }
 
-func main() {
-	var cmd string = "uptime"
-	fmt.Println("you will exec command:", cmd)
+// func main() {
+// 	var cmd string = "uptime"
+// 	fmt.Println("you will exec command:", cmd)
 
-	exec.Command(cmd).Run()
+// 	exec.Command(cmd).Run()
+// }
+
+func main5() {
+	var cmd1 = "cale"
+	var cmd2 = "哈哈"
+
+	fmt.Printf("%T %T", cmd1, cmd2)
+}
+
+func main6() {
+	var mystr01 string = "hello"
+	var mystr02 [5]byte = [5]byte{104, 101, 108, 108, 111}
+	fmt.Printf("mystr01: %s\n", mystr01)
+	fmt.Printf("mystr02: %s", mystr02)
+}
+
+func main7() {
+	var country string = "hello,中国" // utf-8编写，英文字母和符合占用1个字节,汉字占三个字节
+	fmt.Println(len(country))
+}
+
+func main8() {
+	var ch = 'A'
+	var chstr = "A"
+	fmt.Printf("%T | %T\n", ch, chstr) // int32 | string
+	fmt.Println(unsafe.Sizeof(ch))     // 4 int32
+	fmt.Println(unsafe.Sizeof(chstr))  // 16
+	fmt.Println(len(chstr))
+	fmt.Printf("%c\n", chstr[0]) // A
+	// chstr[0] = 'x'  // cannot assign to chstr[0]
+	chstr = "XYZ" // 字符串可以修改跳到字符串地址，不可修改字符
+	fmt.Println(chstr)
+	fmt.Printf("%c %d\n", chstr[0], chstr[0]) // X 88
+	fmt.Printf("%c %d\n", chstr[1], chstr[1]) // Y 89
+}
+
+func main() {
+	var mystr01 string = "\\r\\n"
+	var mystr02 string = `\r\n`  // 无转义字符串
+	var mystr03 string = `您好啊!`  // 自带换行符
+	fmt.Println(mystr01)         // \r\n
+	fmt.Println(mystr02)         // \r\n
+	fmt.Printf(" %q\n", mystr01) //  "\\r\\n" 原始字符串
+	fmt.Println(mystr03)
+
 }
